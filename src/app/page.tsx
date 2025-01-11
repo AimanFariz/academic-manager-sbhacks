08a0200824d68087bfb653a4a9c0e8221f0af4ac
Aiman Fariz <99475959+AimanFariz@users.noreply.github.com>
2025-01-11 14:55:01 -0800

    sign in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,19 @@
 import FileUpload from "@/components/FileUpload";
+import SignUp from "@/components/SignUp";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
+    <div className="flex items-center justify-center max-h-screen py-10 gap-48 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/logo-png.webp"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={250}
+          height={50}
           priority
         />
-        <FileUpload/>
-      </main>
-      
+      <SignUp/>
     </div>
   );
 }
