4d19ac56f6ff9a2d922a52243bba33560bf511e5
Anshul Chennavaram <anshul@chennavaram.com>
2025-01-11 21:38:25 -0800

    lots of changes, I don't even remember.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,29 @@
-'use client';
-
-import React, {useEffect} from "react";
-import ReactDOM from "react-dom/client";
-import Calendar from "../components/Calendar";
-import "./globals.css";
+'use client'
+import DemoApp from "@/components/Calendar";
+import CalendarComponent from "@/components/EventCalendar";
+import FetchPhotosButton from "@/components/FetchPhotosButton";
+import FileUpload from "@/components/FileUpload";
+import Image from "next/image";
+import PhotoAnalysis from '@/components/PhotoAnalysis';
 
 
 export default function Home() {
-  useEffect(() => {
-    const rootElement = document.createElement("div");
-    document.body.appendChild(rootElement);
-    const root = ReactDOM.createRoot(rootElement);
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <FileUpload/>
+        <PhotoAnalysis />
 
-    root.render(
-      <React.StrictMode>
-        <Calendar />
-      </React.StrictMode>
-    );
-  }, []);
+      </main>
+      
+    </div>
+  );
 }
